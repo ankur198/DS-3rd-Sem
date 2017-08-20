8da3fb9d4b579ff82d57a40385f73a39465386b2
Ankur Nigam <ankur.nigam198@live.com>
2017-08-20 22:24:47 +0530

    stack menu

---
+++ Stack/Stack.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main() 
+{
+	int choice;
+	do
+	{
+		printf("Enter a choice:");
+		printf("\n1.hjgjhg\n2.\n3.\n4 Exit");
+		scanf("%d", &choice);
+		switch (choice)
+		{
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		default:
+			break;
+		}
+	} while (choice!=4);
+}
